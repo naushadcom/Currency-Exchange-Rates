1d06e1fc6ef15648b4836bb3e5c336a33cb91f9a
Naushad <17naushad2002@gmail.com>
2022-10-30 23:15:25 +0530

    push

--- currency_exchange_rate/src/components/Currency.jsx
+++ currency_exchange_rate/src/components/Currency.jsx
@@ -63,3 +63,75 @@ const Currency =() => {
          </div>)
 }
 export default Currency;
+
+
+
+
+
+// const Currency =() => {
+//   const [data, setData] = useState({
+//       labels:['INR','USD', 'MXN', 'EUR', 'MZN'],
+//       datasets: [
+//         {
+//           label: 'Dataset 1',
+//           data:[],
+//           borderColor: 'rgb(255, 99, 132)',
+//           backgroundColor: 'rgba(25, 90, 13, 0.5)',
+//         },
+//         {
+//           label: 'Dataset 2',
+//           data:[],
+//           borderColor: 'rgb(53, 162, 235)',
+//           backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//         },
+//       ],
+//     });
+//   useEffect(()=> {
+//      const fetchData= async()=> {
+//          const url = 'https://api.exchangerate.host/latest'
+//          const labelSet = []
+//          const dataSet1 = [];
+//          const dataSet2 = [];
+//        await fetch(url).then((data)=> {
+//            console.log("Api data", data)
+//            const res = data.json();
+//            return res
+//        }).then((res) => {
+//            console.log("ressss", res)
+//           for (const val of res) {
+//           }
+//           setData({
+//               labels:['INR','USD', 'MXN', 'EUR', 'MZN'],
+//               datasets: [
+//                 {
+//                   label: 'Dataset ID',
+//                   data:dataSet1,
+//                   borderColor: 'rgb(255, 99, 132)',
+//                   backgroundColor: 'rgba(99, 132, 0.5)',
+//                 },
+//                 {
+//                   label: 'Dataset ID2',
+//                   data:dataSet2,
+//                   borderColor: 'rgb(53, 162, 235)',
+//                   backgroundColor: 'rgba(53, 235, 0.5)',
+//                 },
+//               ],
+//             })
+//           console.log("arrData", dataSet1, dataSet2)
+//        }).catch(e => {
+//               console.log("error", e)
+//           })
+//       }
+      
+//       fetchData();
+//   },[])
+ 
+//   return(
+//       <div style={{width:'80%', height:'50%'}}>
+//           {
+//               console.log("dataaaaaaaa", data)
+//           }
+//           <Bar data={data} options={options}/>
+//        </div>)
+// }
+// export default Currency;
